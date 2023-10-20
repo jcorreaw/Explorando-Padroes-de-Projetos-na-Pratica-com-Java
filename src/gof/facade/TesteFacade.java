453b289c06002a599f40626ef11e6107e3458753
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-20 13:51:46 -0300

    Facade

---
+++ src/gof/facade/TesteFacade.java
@@ -0,0 +1,8 @@
+package gof.facade;
+
+public class TesteFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Joao", "27252490");
+    }
+}
